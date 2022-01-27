4acafa0dea2314a21927b40e92970ca2399e75d9
Liam Knapp <Lknapp1110@conestogac.on.ca>
2022-01-27 10:17:15 -0500

    Add project files.

---
+++ OOP - Lab2/main.cpp
@@ -0,0 +1,30 @@
+//Liam Knapp
+//OOP Lab 2
+
+
+#include <iostream>
+using namespace std;
+
+#define MAXSIZE 50
+
+class Truck {
+private:
+	int x;
+	int y;
+	int truckID;
+
+public:
+
+};
+
+class Driver {
+private:
+	char driver_first_name[MAXSIZE];
+	char driver_last_name[MAXSIZE];
+	int driverID;
+};
+
+int main() {
+	Truck t1;
+	Truck t2;
+}
